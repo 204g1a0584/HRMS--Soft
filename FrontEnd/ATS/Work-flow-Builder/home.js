b42b4b8b4386b0f086ac6fe14a9491a99fc784ce
SAI CHARAN REDDY <79386814+204g1a0584@users.noreply.github.com>
2023-11-03 15:32:12 +0530

    linking blocks functional

--- FrontEnd/ATS/Work-flow-Builder/home.js
+++ FrontEnd/ATS/Work-flow-Builder/home.js
@@ -1,42 +1,36 @@
-// This function will run when the drag starts.
-function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
+function allowDrop(ev) {
+    ev.preventDefault();
 }
 
-// This function runs when the dragged item enters the drop zone.
-function dragEnter(e) {
-    e.preventDefault();
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
-// This function runs when the dragged item is over the drop zone.
-function dragOver(e) {
-    e.preventDefault();
+function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    const nodeCopy = document.getElementById(data).cloneNode(true);
+    nodeCopy.id = data + "_copy";
+    nodeCopy.draggable = false; // to make it non-draggable after placing in the canvas
+    ev.target.appendChild(nodeCopy);
+    populatePropertiesPanel(data);
 }
 
-// This function runs when the dragged item is dropped into the drop zone.
-function drop(e) {
-    e.preventDefault();
-    
-    // Get the id of the dragged element
-    const id = e.dataTransfer.getData('text/plain');
-    const draggedElement = document.getElementById(id);
-    
-    // Clone the dragged element
-    const clone = draggedElement.cloneNode(true);
-    
-    // Generate a unique id for the cloned element
-    clone.id = 'clone-' + new Date().getTime();
-    
-    // Append the cloned element to the canvas
-    document.getElementById('canvas').appendChild(clone);
+function populatePropertiesPanel(stepId) {
+    const propertiesPanel = document.querySelector('.properties-panel');
+    propertiesPanel.innerHTML = '<h4>Properties Panel</h4>'; // reset content
+    let content = '<label>Step Name:</label><input type="text">';
+    switch(stepId) {
+        case 'candidate':
+            content += '<label>Step Description:</label><input type="text" value="Name of Candidate">';
+            break;
+        case 'hr':
+            content += '<label>Step Description:</label><input type="text" value="Assigned HR">';
+            break;
+        case 'comment':
+            content += '<label>Step Description:</label><textarea>Comments</textarea>';
+            break;
+    }
+    content += '<label>Step Configuration:</label><textarea></textarea>';
+    propertiesPanel.innerHTML += content;
 }
-
-// Assign event listeners
-document.querySelectorAll('.step').forEach(item => {
-    item.setAttribute('draggable', 'true');
-    item.addEventListener('dragstart', dragStart);
-});
-
-document.getElementById('canvas').addEventListener('dragenter', dragEnter);
-document.getElementById('canvas').addEventListener('dragover', dragOver);
-document.getElementById('canvas').addEventListener('drop', drop);
